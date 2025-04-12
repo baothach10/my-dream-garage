73f12948ce7d12eb8a016fc0e9b9b2d71478ab0a
baothach10 <baothach10@gmail.com>
2025-04-12 15:13:45 +0700

    feat: initialize source code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Dream Garage</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type TSpecification = {
+    name: string;
+    year: number;
+    price: string;
+    engine: string;
+    horsepower: number;
+    fuelType: string;
+    transmission: string;
+    topSpeed: string;
+    acceleration: string;
+    features: string[];
+    image?: string;
+    video?: string;
+}
